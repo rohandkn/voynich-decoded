45f79f11ffc61c510001f74c63d1b6939b04fd73
Matthew Kuczynski <mkucz018@gmail.com>
2021-11-02 23:31:51 -0400

    add train and metrics

--- run.py
+++ run.py
@@ -54,11 +54,43 @@ class LSTM(nn.Module):
 		return out
 
 def train_model(model, epochs, lr):
+	parameters = filter(lambda p: p.requires_grad, model.parameters())
 	optimizer = torch.optim.Adam(parameters, lr=lr)
 	for i in range(epochs):
 		model.train()
+		sum_loss = 0.0
+		total = 0
+		for x, y, l in train_dl:
+			x = x.long()
+			y = y.long()
+			y_pred = model(x, l)
+			optimizer.zero_grad()
+			loss = F.cross_entropy(y_pred, y)
+			loss.backward()
+			optimizer.step()
+			sum_loss += loss.item()*y.shape[0]
+			total += y.shape[0]
+		val_loss, val_acc, val_rmse = validation_metrics(model, val_dl)
+		if i % 5 == 1:
+			print("train loss %.3f, val loss %.3f, val accuracy %.3f, and val rmse %.3f" % (sum_loss/total, val_loss, val_acc, val_rmse))
 
-
+def validation_metrics(model, valid_dl):
+	model.eval()
+	correct = 0
+	total = 0
+	sum_loss = 0.0
+	sum_rmse = 0.0
+	for x, y, l in valid_dl:
+		x = x.long()
+		y = y.long()
+		y_hat = model(x, l)
+		loss = F.cross_entropy(y_hat, y)
+		pred = torch.max(y_hat, 1)[1]
+		correct += (pred == y).float().sum()
+		total += y.shape[0]
+		sum_loss += loss.item()*y.shape[0]
+		sum_rmse += np.sqrt(mean_squared_error(pred, y.unsqueeze(-1)))*y.shape[0]
+	return sum_loss/total, correct/total, sum_rmse/total
 
 vm = VoynichManuscript("voynich-text.txt", inline_comments=False)
 train = VoynichDataset()
