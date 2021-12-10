81fce4ccdb1a0e00a0f68665869dc765d5eaec82
Matthew Kuczynski <mkucz018@gmail.com>
2021-12-10 14:31:12 -0500

    shap

--- bert_kfold.py
+++ bert_kfold.py
@@ -144,7 +144,7 @@ def Bert():
   
 
   pdb.set_trace()
-  model = torch.load("bestmodel.rpt") #BertForSequenceClassification.from_pretrained("bestmodel.rpt", num_labels=6)
+  model = torch.load("bestmodel.rpt", map_location=torch.device('cpu')) #BertForSequenceClassification.from_pretrained("bestmodel.rpt", num_labels=6)
   model.to(device)
 
   param_optimizer = list(model.named_parameters())
