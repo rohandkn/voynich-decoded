fa458eba73a231c0a80a82b3eae88832394e102e
Matthew Kuczynski <mkucz018@gmail.com>
2021-12-10 22:31:57 -0500

    shap

--- get_shap.py
+++ get_shap.py
@@ -5,8 +5,8 @@ a = np.load("shap_sum.npy", allow_pickle=True)
 div_list = []
 for i in range(6):
 	b = {}
-	for k in a[0].keys():
-		b[k] = a[0][k] / a[6][k]
+	for k in a[i].keys():
+		b[k] = a[i][k] / a[6][k]
 	div_list.append({k: v for k, v in sorted(b.items(), key=lambda item: item[1])})
 
 pdb.set_trace()
